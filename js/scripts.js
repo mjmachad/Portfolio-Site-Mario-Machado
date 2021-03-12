bfc41b5436a644c0e0eb32d989221411d1b7fc03
mjmachad <>
2021-03-12 14:29:42 -0500

    If applied this commit will apply the JS coding to add functionality to the menu icon

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hello Welcome to my Portfolio");
+
+function myfunction() {
+  var x = document.getElementById("myNavtoggle")
+  if (x.className === "navtoggle"){
+    x.className +=  " responsive";
+  } else{
+    x.className = "navtoggle";
+  }
+}
